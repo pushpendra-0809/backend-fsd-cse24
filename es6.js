8c35f029f083c527505061b5e716dc5c4fe1c323
Pushpendra Kumar Verma <vermapushpendra0809@gmail.com>
2026-07-23 12:46:16 +0530

    21-07-2026

---
+++ es6.js
@@ -0,0 +1,12 @@
+//js script
+//function : reusuable block of code
+//function calling
+function sayHello(a,b)
+{
+    console.log("Hello");
+    console.log(a+b);
+}
+sayHello(3,2);
+const hello=()=>{
+    console.log("Monday");
+}
